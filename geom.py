7b798ccc7e4a7f969bfdc0c73048c6aefe6330f6
Justica <atsuronreplaced@gmail.com>
2023-11-23 23:47:16 +0900

    Impl SmoothSurface

--- geom.py
+++ geom.py
@@ -1,6 +1,8 @@
-import abc
+from typing import Iterable
 
-import matplotlib.pyplot as plt
+import abc
+import uuid
+
 import numpy as np
 
 
@@ -9,10 +11,27 @@ def normalize(vec: np.ndarray) -> np.ndarray:
 
 
 class Particle:
-    def __init__(self, pos: np.ndarray, vec: np.ndarray, intensity: float = 1.0):
+    def __init__(self, pos: np.ndarray, vec: np.ndarray,
+                 parent_ids: list[str] = None, intensity: float = 1.0):
+        self._uuid = uuid.uuid1()
         self._pos = pos
         self._vec = vec
         self._intensity = intensity
+        self._terminated = False
+
+        if parent_ids is None:
+            self._parents_ids = []
+            self._generation = 1
+        else:
+            self._parents_ids = parent_ids
+            self._generation = 1 + len(parent_ids)
+
+    def __str__(self) -> str:
+        p = self._fmt_list(self._pos)
+        v = self._fmt_list(self._vec)
+        i = self._fmt(self._intensity)
+        g = self._fmt(self._generation)
+        return f"Particle(pos: {p}, vec: {v}, itst: {i}, gen: {g})"
 
     def get_pos(self) -> np.ndarray:
         return self._pos
@@ -22,6 +41,33 @@ class Particle:
 
     def get_intensity(self) -> float:
         return self._intensity
+
+    def get_id(self) -> str:
+        return str(self._uuid)
+
+    def get_ids_with_self(self) -> list[str]:
+        return self._parents_ids + [self.get_id()]
+
+    def get_generation(self) -> int:
+        return self._generation
+
+    def set_terminated(self, is_terminated: bool):
+        self._terminated = is_terminated
+
+    @staticmethod
+    def _fmt(val: int | float) -> str:
+        if type(val) is int:
+            return str(val)
+
+        val_str = f"{val:.3f}"
+        if val >= 0:
+            return " " + val_str
+        return val_str
+
+    @staticmethod
+    def _fmt_list(vals: Iterable) -> str:
+        str_list = [Particle._fmt(v) for v in vals]
+        return "[" + ", ".join(str_list) + "]"
 
 
 class Surface(metaclass=abc.ABCMeta):
@@ -43,13 +89,16 @@ class Surface(metaclass=abc.ABCMeta):
         return self._index
 
     @abc.abstractmethod
-    def get_collision_particle(self, num: int, collision_angle: float) -> list[Particle]:
+    def get_collision_particle(self, num: int, in_part: Particle, collision_angle: float) -> list[Particle]:
         pass
 
 
 class SmoothSurface(Surface):
-    def get_collision_particle(self, num: int, collision_angle: float) -> list[Particle]:
-        pass
+    def get_collision_particle(self, in_part: Particle, num: int, c_param: np.ndarray) -> list[Particle]:
+        e1, e2, c = c_param
+        c_point = np.sum(self.get_basis() * np.array([[e1], [e2]]), axis=0) + self.get_origin()
+        out_vec = calc_main_out_vec(self, in_part)
+        return [Particle(c_point, out_vec, in_part.get_ids_with_self(), in_part.get_intensity())]
 
 
 def calc_collision_param(suf: Surface, part: Particle) -> np.ndarray:
@@ -70,18 +119,19 @@ def do_collision(c_param: np.ndarray) -> bool:
     return True
 
 
-def calc_collision_angle(suf: Surface, part: Particle) -> float:
-    norm = suf.get_norm_vec()
-    vec = part.get_vec()
-    cos_theta = np.dot(norm, vec) / np.linalg.norm(norm) / np.linalg.norm(vec)
-    return np.arccos(np.abs(cos_theta))
+# def calc_collision_angle(suf: Surface, part: Particle) -> float:
+#     norm = suf.get_norm_vec()
+#     vec = part.get_vec()
+#     cos_theta = np.dot(norm, vec) / np.linalg.norm(norm) / np.linalg.norm(vec)
+#     return np.arccos(np.abs(cos_theta))
 
 
 def rotate_vector(vec: np.ndarray, normalized_axial: np.ndarray, radian: float) -> np.ndarray:
+    # Reference: http://www.info.hiroshima-cu.ac.jp/~miyazaki/knowledge/tech0007.html
     r = np.array([
         [0, -normalized_axial[2], normalized_axial[1]],
-        [normalized_axial[2], 0,  -normalized_axial[0]],
-        [-normalized_axial[2], normalized_axial[0], 0]
+        [normalized_axial[2], 0, -normalized_axial[0]],
+        [-normalized_axial[1], normalized_axial[0], 0]
     ])
     m = np.eye(3) + np.sin(radian) * r + (1 - np.cos(radian)) * np.dot(r, r)
     qm = np.eye(4)
@@ -89,54 +139,53 @@ def rotate_vector(vec: np.ndarray, normalized_axial: np.ndarray, radian: float) 
 
     qv = np.ones(4)
     qv[0:3] = vec
-    print(np.linalg.det(qm))
     return np.dot(qm, qv)[0:3]
 
 
-def calc_main_out_vec(suf: Surface, part: Particle, c_param: np.ndarray) -> np.ndarray:
-    # Reference: http://www.info.hiroshima-cu.ac.jp/~miyazaki/knowledge/tech0007.html
-    e1, e2, c = c_param
-    c_point = np.sum(suf.get_basis() * np.array([[e1], [e2]]), axis=0) + suf.get_origin()
+def calc_main_out_vec(suf: Surface, part: Particle) -> np.ndarray:
     norm = normalize(suf.get_norm_vec())
+
+    in_vec = -part.get_vec()  # inverse direction
+    return rotate_vector(in_vec, norm, np.pi)
 
 
 if __name__ == "__main__":
     import importlib
     import plot
+
     importlib.reload(plot)
 
-    # surface = SmoothSurface(
-    #     1111,
-    #     np.array([0, 1, 0]),
-    #     np.array([0, 1, 1]),
-    #     np.array([100, 1, 0]),
-    # )
-    #
-    # particles = []
-    # angles = []
-    # while len(particles) < 20:
-    #     particle = Particle(np.zeros(3), np.random.random(3))
-    #
-    #     collision_param = calc_collision_param(surface, particle)
-    #     if do_collision(collision_param):
-    #         ang = calc_collision_angle(surface, particle)
-    #         particles.append(particle)
-    #         angles.append(ang)
-    #
-    #         ret = calc_main_out_vec(surface, particle, collision_param)
+    surface = SmoothSurface(
+        1111,
+        np.array([0, 1, 0]),
+        np.array([0, 1, 1]),
+        np.array([100, 1, 0]),
+    )
 
-    n = normalize(np.ones(3))
-    v = normalize(np.random.random(3))
-    vectors = []
-    for ang in np.linspace(0, np.pi, 20):
-        rv = rotate_vector(v, n, ang)
-        vectors.append(rv)
+    particles = [Particle(np.zeros(3), np.random.random(3)) for _ in range(20)]
+    child_particles = []
+    for particle in particles:
+        collision_param = calc_collision_param(surface, particle)
+        if do_collision(collision_param):
+            child_particles += surface.get_collision_particle(particle, 1, collision_param)
 
-    colors = ["red"] * len(vectors)
-    colors.append("k")
-
-    vectors.append(n)
-    vectors = np.array(vectors)
-    locations = np.zeros((len(vectors), 3))
-
+    colors = ["red"] * len(particles) + ["green"] * len(child_particles)
+    vectors = np.array([p.get_vec() for p in particles + child_particles])
+    locations = np.array([p.get_pos() for p in particles + child_particles])
     plot.vector(vectors, locations, colors)
+
+    # n = normalize(np.ones(3))
+    # v = normalize(np.random.random(3))
+    # vectors = []
+    # for ang in np.linspace(0, np.pi, 20):
+    #     rv = rotate_vector(v, n, ang)
+    #     vectors.append(rv)
+    #
+    # colors = ["red"] * len(vectors)
+    # colors.append("k")
+    #
+    # vectors.append(n)
+    # vectors = np.array(vectors)
+    # locations = np.zeros((len(vectors), 3))
+    #
+    # plot.vector(vectors, locations, colors)
